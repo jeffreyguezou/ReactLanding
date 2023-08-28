4f3bbf971039c5093b9870a3c19e6d8d680a8420
Jeffrey Raj Guezou <41725439+jeffreyguezou@users.noreply.github.com>
2023-08-28 11:58:44 +0530

    Update Button.js

--- src/UI/Button.js
+++ src/UI/Button.js
@@ -1,9 +1,24 @@
-import classes from './Button.module.css'
+import { useState } from "react";
+import classes from "./Button.module.css";
 function Button() {
-    return (
-        <div className={classes.btnSection}>
-            <button className={classes.bookBtn}>Explore</button>
-        </div>
-    )
+  const [isBtnHover, setIsBTnHover] = useState(false);
+  let styleClass = isBtnHover ? "bookBtnHovered" : "bookBtn";
+  const btnHoverHandler = () => {
+    setIsBTnHover(true);
+  };
+  const btnRemoveHoverHandler = () => {
+    setIsBTnHover(false);
+  };
+  return (
+    <div className={classes.btnSection}>
+      <button
+        onMouseEnter={btnHoverHandler}
+        onMouseLeave={btnRemoveHoverHandler}
+        className={styleClass}
+      >
+        Explore
+      </button>
+    </div>
+  );
 }
-export default Button
+export default Button;
